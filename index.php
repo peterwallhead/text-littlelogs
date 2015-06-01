9f7e1275677aaace71c7feb52463b1ad4dc94c43
Peter Wallhead <peter.wallhead@gmail.com>
2015-06-01 18:12:46 +1000

    Pre-emptive spammy scaper prevention

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-  $from = $_REQUEST['From'];
+	$from = $_REQUEST['From'];
 	$body = $_REQUEST['Body'];
 
-  //This stops randoms using your Twilio budget (you should probably not tell people your Twilio number anyway)
+  	//This stops randoms using your Twilio budget (you should probably not tell people your Twilio number anyway)
 	if($from != "+61427123456") { //Replace this number with your own mobile number, including the international code
 		exit;
 	}
@@ -11,7 +11,7 @@
     	$body = preg_replace('/\{(littlelog)\}/usi','', $body);
     	$body = trim($body);
     	
-    	$email_to = "your-secret-email@mailbot.littlelogs.co"; //Replace this with your own secret littlelogs address
+    	$email_to = "your-secret-email"; //Replace this with your own secret littlelogs address
 
     	mail($email_to, "This is a littlelog", $body);
 
@@ -21,7 +21,7 @@
 
 	header("content-type: text/xml");
 	
-	//The XML response below is optional, because it will text you back whatever is in $response (and this will cost you money).
+//The XML response below is optional, because it will text you back whatever is in $response (and this will cost you money).
 ?>
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
